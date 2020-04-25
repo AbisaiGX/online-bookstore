ff7daa14735c0ab410399cfc7f0a5767e13483bd
Abisai Gonzalez <pachecoabisai@gmail.com>
2020-04-25 18:58:14 -0500

    Creación de la entidad Book y entidad Category con sus respectivos repositorios.

---
+++ onlinebookstore/src/main/java/in/bushansirgur/onlinebookstore/repository/BookCategoryRepository.java
@@ -0,0 +1,9 @@
+package in.bushansirgur.onlinebookstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.bushansirgur.onlinebookstore.entity.BookCategory;
+
+public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
+
+}
